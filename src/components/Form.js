f3c33215fbaf7deb45331bd8cf68c921bcb3a9b2
Rafal <r.porzucek@gmail.com>
2018-09-29 13:29:14 +0100

    Added components

---
+++ src/components/Form.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+
+export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...this.props.initialState
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState(prevState => ({
+      ...prevState,
+      [event.target.name]: event.target.value
+    }));
+  }
+
+  render() {
+    return (
+      <form onChange={this.handleChange(event)}>
+        <div>
+          <label>Due Date</label>
+          <input name="dueDate" type="text" defaultValue={this.state.dueDate} />
+        </div>
+        <div>
+          <label> Invoice Value </label>
+          <input
+            name="invoiceValue"
+            type="text"
+            defaultValue={this.state.invoiceValue}
+          />
+        </div>
+        <div>
+          <label> Customer </label>
+          <input
+            name="customer"
+            type="text"
+            defaultValue={this.state.customer}
+          />
+        </div>
+        <div>
+          <label> Description </label>
+          <input
+            name="description"
+            type="text"
+            defaultValue={this.state.description}
+          />
+        </div>
+        <button> Add invoice </button>
+      </form>
+    );
+  }
+}
---
+++ src/components/InvoiceList.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class InvoiceList extends Component {
+  render() {
+    return (
+      <div>
+        {Object.entries(this.props.invoices).map(([id, invoice]) => (
+          <div key={id}>
+            <div>{invoice.id}</div>
+            <div>{invoice.customer}</div>
+            <div>{invoice.invoiceValue}</div>
+            <div>{invoice.dueDate}</div>
+            <div>{invoice.description}</div>
+            <div>{invoice.isPaid}</div>
+            <button onclick={() => this.markAsPaid(id)}> Mark as paid</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
