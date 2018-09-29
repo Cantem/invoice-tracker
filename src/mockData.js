00e7105ff0795e5453067a1e3ba2ce2d6d4011db
Rafal <r.porzucek@gmail.com>
2018-09-29 12:58:08 +0100

    Added mock data

---
+++ src/mockData.js
@@ -0,0 +1,34 @@
+export const mockCustomerData = {
+  "1": {
+    id: "1",
+    dueDate: "20-10-2018",
+    invoiceValue: "99.27",
+    customer: "Terry Ltd",
+    description: "Building materials",
+    isPaid: null
+  },
+  "2": {
+    id: "2",
+    dueDate: "30-09-2018",
+    invoiceValue: "25.27",
+    customer: "Andres",
+    description: "White shelf",
+    isPaid: "19-09-2018"
+  },
+  "3": {
+    id: "3",
+    dueDate: "30-10-2018",
+    invoiceValue: "109.27",
+    customer: "Office Ltd",
+    description: "Office materials",
+    isPaid: null
+  },
+  "4": {
+    id: "4",
+    dueDate: "27-10-2018",
+    invoiceValue: "9.70",
+    customer: "Rafal Porzucek",
+    description: "Flat white x3",
+    isPaid: null
+  }
+};
