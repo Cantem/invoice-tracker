0ef03a24b3631dde8b312ed0c10b4422cef6d798
Rafal <r.porzucek@gmail.com>
2018-09-29 13:02:43 +0100

    Added initial state

--- src/index.js
+++ src/index.js
@@ -3,4 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { mockCustomerData } from "./mockData";
+
+const initialState = {
+  dueDate: "today",
+  invoiceValue: "100000.00",
+  customer: "Apple",
+  description: "loads of services",
+  isPaid: ""
+};
+
+ReactDOM.render(
+  <App initialState={initialState} mockCustomerData={mockCustomerData} />,
+  document.getElementById("root")
+);
